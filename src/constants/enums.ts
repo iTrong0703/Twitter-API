4580451eb8c473c00cad30f19880af8ea14b74d6
Nguyen Thanh Trong <ngthtrong0354@gmail.com>
2024-09-04 01:11:23 +0700

    Add functions for generating Access Token and Refresh Token

--- src/constants/enums.ts
+++ src/constants/enums.ts
@@ -1,5 +1,14 @@
+// Loại tài khoản
 export enum UserVerifyStatus {
   Unverified, // Chưa xác thực email
   Verified, // Đã xác thực email
   Banned // Bị ban
+}
+
+// Loại token
+export enum TokenType {
+  AccessToken,
+  RefreshToken,
+  ForgotPasswordToken,
+  VerifyEmailToken
 }
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -2,9 +2,36 @@ import User from '~/models/schemas/User.schema'
 import databaseService from './database.services'
 import { RegisterRequestBody } from '~/models/requests/User.requests'
 import { hashPassword } from '~/utils/hash'
+import { signToken } from '~/utils/jwt'
+import { TokenType } from '~/constants/enums'
 
 class UsersService {
+  // Tạo access token
+  private signAccessToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.AccessToken
+      },
+      options: {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN
+      }
+    })
+  }
+  // Tạo refresh token
+  private signRefreshToken(user_id: string) {
+    return signToken({
+      payload: {
+        user_id,
+        token_type: TokenType.RefreshToken
+      },
+      options: {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN
+      }
+    })
+  }
   async register(payload: RegisterRequestBody) {
+    // Hash password trước khi đưa vào db
     const hashedPassword = await hashPassword(payload.password)
     const result = await databaseService.users.insertOne(
       new User({
@@ -13,7 +40,15 @@ class UsersService {
         password: hashedPassword
       })
     )
-    return result
+    const user_id = result.insertedId.toString()
+    const [acces_token, refresh_token] = await Promise.all([
+      this.signAccessToken(user_id),
+      this.signRefreshToken(user_id)
+    ])
+    return {
+      acces_token,
+      refresh_token
+    }
   }
 
   async checkEmailExist(email: string) {
